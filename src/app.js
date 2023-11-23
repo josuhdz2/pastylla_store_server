857ca123d89e55ba91a11b99c06382fec952bd63
josuhdz2 <josueherdeveloper@gmail.com>
2023-11-23 14:31:03 -0600

    agregacion de software de monitoreo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express=require('express');
+const prom=require('prom-client');
 const mongoose=require('mongoose');
 const cors=require('cors');
 const path=require('path');
@@ -18,6 +19,7 @@ mongoose.connect(process.env.DATABASE_URL)
     console.log('Error with connecting database...\n'+err);
 });
 const app=express();
+prom.collectDefaultMetrics();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
@@ -27,6 +29,16 @@ app.get('/', (req, res)=>
 {
     res.send('Servicio en linea');
 });
+app.get('/metrics', async (req, res) => {
+    try {
+        res.set('Content-Type', prom.register.contentType);
+        const metrics = await prom.register.metrics();
+        res.end(metrics);
+      } catch (err) {
+        console.error('Error al obtener métricas:', err);
+        res.status(500).end();
+      }
+  });
 app.use('/productos', productsRouter);
 app.use('/usuario', userRouter);
 app.use('/admin', adminRouter);
